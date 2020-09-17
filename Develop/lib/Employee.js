7a0f871e94e77f9ac6edc567f1ccf36f3b524cde
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-16 22:54:22 -0500

    added base class of Employee, then added subclasses in their respective .js files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    getName(){
+     return this.name;   
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail(){
+        return this.email;
+    }
+
+    getRole() {
+        return 'Employee';
+    }
+}
+
+// const employee = new Employee("James", 43, "james@company.com");
+// console.log(employee);
+
+module.exports = Employee;
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require('./Employee.js');
+
+class Engineer extends Employee {
+    constructor(name, id, email, github){
+        super(name, id, email);
+        this.github = github;
+    }
+
+    getGithub() {
+        return this.github;
+    }
+
+    getRole() {
+        return 'Engineer';
+    }
+}
+
+// const engineer = new Engineer("John", 12, "john@company.com","jamesMUFN");
+// console.log(engineer);
+
+// console.log(engineer.getRole())
+
+module.exports = Engineer;
