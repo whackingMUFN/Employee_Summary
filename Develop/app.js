09e1081b172e4a41cd40e005425e3cec79786a23
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2020-09-19 19:06:32 -0500

    added gif and screenshot - removed comments

--- Develop/app.js
+++ Develop/app.js
@@ -12,7 +12,7 @@ const render = require("./lib/htmlRenderer");
 
 let arr = [];
 
-console.log(outputPath);
+
 const questions = [
     {
         type: "input",
@@ -58,23 +58,21 @@ const questions = [
         message: 'Would you like to add another employee?',
         name: 'add'
     }
-]
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+];
+
+
 function init() {
     inquirer
     .prompt(questions)
     .then((res) => {
         arr.push(res);
-        console.log(arr);
-        console.log('^^^ arr after push ^^^')
+
         if (res.add) {
-            init()
+            init();
         } else {
            createAndRenderObjects(arr);
         }            
-    })
-    .then()
+    });
 }
 
 
@@ -82,40 +80,22 @@ function createAndRenderObjects(users) {
     let emps = [];
     users.forEach(element => {
         if(element.empType === "Manager") {
-            x = new Manager(element.name, element.id, element.email, element.office)
+            x = new Manager(element.name, element.id, element.email, element.office);
             emps.push(x);
         } else if (element.empType === "Engineer") {
-            y = new Engineer(element.name, element.id, element.email, element.github)
+            y = new Engineer(element.name, element.id, element.email, element.github);
             emps.push(y);
         } else {
-            z = new Intern(element.name, element.id, element.email, element.school)
+            z = new Intern(element.name, element.id, element.email, element.school);
             emps.push(z);
         }
         rendered = render(emps);
         fs.writeFile(outputPath, rendered, (err)=>{
             if (err) throw err;
-            console.log('rendered data has been written');
-        })
+            
+        });
+        console.log('New employess rendered.');
     });
 }
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 init();
